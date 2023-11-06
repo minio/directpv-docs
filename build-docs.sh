43d3c08a6f61afa727246bea71c423ab7a101d0f
Daryl White <53910321+djwfyi@users.noreply.github.com>
2023-11-06 13:08:55 -0500

    Add build-docs.sh file

---
+++ build-docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+export NVM_DIR="${HOME}/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+nvm use stable
+
+export PATH=${PATH}:${HOME}/.local/bin
+
+# Update theme and rebuild theme assets
+git submodule update --init --recursive
+git submodule update --recursive --remote
+cd themes/kes-docs-theme
+npm install && npm run build
+cd ../../
+
+# Build the doc site
+hugo
+
+# Remove old site and copy new site contents over to path
+sudo rm -rf /var/www/docs/directpv/
+sudo mkdir -p /var/www/docs/directpv/
+sudo cp -vr public/* /var/www/docs/directpv/
